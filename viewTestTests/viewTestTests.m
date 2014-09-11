99565a0b594aacf7bbdf79a095a7da07ff938d77
Paula Yuan <pyuan@amazonia.cs.hmc.edu>
2014-09-10 18:24:30 -0700

    Initial Commit

---
+++ viewTestTests/viewTestTests.m
@@ -0,0 +1,34 @@
+//
+//  viewTestTests.m
+//  viewTestTests
+//
+//  Created by Paula Yuan on 9/10/14.
+//  Copyright (c) 2014 Paula Yuan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface viewTestTests : XCTestCase
+
+@end
+
+@implementation viewTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
